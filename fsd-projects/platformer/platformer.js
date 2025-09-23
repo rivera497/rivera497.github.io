c236e705e8dd6534b1e050142dafd09e4066bbf2
rivera497 <rivera497@wearescpps.org>
2025-09-23 16:33:40 +0000

    fixed problems; co-pilot led her astray

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+  toggleGrid();
 
 
-    // TODO 2 - Create Platforms
+  // TODO 2 - Create Platforms
+  createPlatform(400, 700, 200, 10, "brown");
+  createPlatform(500, 600, 100, 10, "brown");
+  createPlatform(630, 500, 100, 210, "grey");
+  createPlatform(700, 10, 1000, 210, "grey");
+  createPlatform(1121, 700, 70, 40, "blue");
+  createPlatform(1221, 600, 70, 40, "blue");
+  createPlatform(1370, 400, 100, 10, "red");
+  createPlatform(1340, 500, 100, 10, "yellow");
+  createPlatform(1300, 600, 100, 10, "lime");
 
 
+  // TODO 3 - Create Collectables
+  createCollectable("steve", 700, 430);
+  createCollectable("kennedi", 200, 170, 0.5, 0.7);
+  createCollectable("database", 1173, 600);
+  createCollectable("diamond", 1200, 320, 0, 0.7, 1000, 1200, 3);
+  createCollectable("max", 1356, 430, 0.5, 0.7);
 
 
-    // TODO 3 - Create Collectables
-
-
-
-    
-    // TODO 4 - Create Cannons
+  // TODO 4 - Create Cannons
+  createCannon("left", 400, 600);
+  createCannon("top", 400, 500);
+  createCannon("right", 300, 500);
 
 
     
